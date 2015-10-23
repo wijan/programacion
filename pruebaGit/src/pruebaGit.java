a443f3e9883b27ff82bfa7fbb5ec5211f480eaa9
wijan <wiji180892@gmail.com>
2015-10-23 04:49:16 +0200

    añadida la prueba

---
+++ pruebaGit/src/pruebaGit.java
@@ -0,0 +1,12 @@
+
+public class pruebaGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Prueba de Git");
+	}
+
+}
